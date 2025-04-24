8a2aa6b812966173e4fae248a39025a575059938
lwjmcn <clarecse02@gmail.com>
2025-04-25 00:11:39 +0900

    feat: setup w/ react, typescript, vite, eslint

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import legacy from "@vitejs/plugin-legacy";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    legacy({
+      targets: ["defaults", "ie >= 11"],
+      polyfills: true,
+      modernPolyfills: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  resolve: { alias: { "@": new URL("./src", import.meta.url).pathname } },
+});

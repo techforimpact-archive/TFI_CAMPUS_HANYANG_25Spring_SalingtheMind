283dac3c9a4af03c3965e97f4857785c54a4864c
lwjmcn <clarecse02@gmail.com>
2025-05-09 01:09:33 +0900

    feat: signin page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,11 +1,32 @@
+import { useAuthStore } from '@/store/auth';
+import { Link, replace, useNavigate } from 'react-router-dom';
+import styles from './signin.module.css';
+
 export default function SignInPage() {
+  const navigate = useNavigate();
+  const { isAuth, login } = useAuthStore();
+
+  const handleLogin = () => {
+    console.log('Login button clicked');
+
+    // Handle login logic here
+
+    console.log('Login successful');
+    login();
+    navigate('/', { replace: true });
+  };
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Login Page</h1>
-      <p>Please log in to continue.</p>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button style={{ margin: '5px' }}>Login</button>
+    <div className={styles.container}>
+      <h1>마음의 항해</h1>
+      <img className={styles.logo} src="https://placehold.co/400x400" alt="" />
+      <input className={styles.nicknameInput} type="text" placeholder="닉네임" />
+      <button className={styles.loginButton} onClick={handleLogin}>
+        Login
+      </button>
+      <Link to="/signup" style={{ display: 'block', margin: '10px 0' }}>
+        회원가입
+      </Link>
     </div>
   );
 }

eaf750cb0dc8067adf1522f59696f0e5dd6d7428
Sunmin-Lim <165561342+Sunmin-Lim@users.noreply.github.com>
2025-05-12 18:40:54 +0900

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -10,7 +10,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // 또는 쿠키 등에서
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }

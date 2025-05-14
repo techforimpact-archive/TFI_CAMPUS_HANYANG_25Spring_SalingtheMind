60c42848e1a3814be3134fc74954b3b298f98028
han-chaelin <chelin02@naver.com>
2025-05-15 00:39:04 +0900

    render 오류 해결 코드

--- utils/db.py
+++ utils/db.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+"""from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
@@ -11,5 +11,17 @@ if not MONGO_URI:
     raise Exception("MONGO_URI가 .env에 설정되지 않았습니다.")
 
 # MongoDB 클라이언트 생성
+client = MongoClient(MONGO_URI)
+db = client.get_default_database()  # 기본 데이터베이스 사용"""
+
+from pymongo import MongoClient
+import os
+
+# 로컬 개발 환경에서만 dotenv 사용
+if os.environ.get("FLASK_ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client.get_default_database()  # 기본 데이터베이스 사용

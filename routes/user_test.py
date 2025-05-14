926f52ade67b110efdc6f570f7773d0f60b75838
rbanbla <kkhhyy25@hanmail.net>
2025-05-14 19:41:14 +0900

    cookie 추가

--- routes/user_test.py
+++ routes/user_test.py
@@ -1,6 +1,6 @@
 import datetime
-from flask import request, Blueprint, Response
-from flasgger import Swagger, swag_from
+from flask import request, Blueprint, Response, make_response
+from flasgger import swag_from
 import jwt
 from bson.objectid import ObjectId
 from functools import wraps
@@ -17,7 +17,7 @@ def json_kor(data, status=200):
         content_type="application/json; charset=utf-8",
         status=status
     )
-    
+
 def create_token(user_doc):
     expire = datetime.datetime.utcnow() + datetime.timedelta(
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
@@ -27,62 +27,10 @@ def create_token(user_doc):
         "nickname": user_doc["nickname"],
         "exp": expire
     }
-    token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
-    return token
-
+    return jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
 @user_test.route('/signup', methods=['POST'])
 def signup():
-    """
-    회원가입
-    ---
-    tags:
-      - User
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          type: object
-          properties:
-            nickname:
-              type: string
-              description: 사용자 닉네임
-            age:
-              type: integer
-              description: 나이
-            gender:
-              type: string
-              description: 성별
-            address:
-              type: string
-              description: 주소
-            phone:
-              type: string
-              description: 전화번호
-          required:
-            - nickname
-            - age
-            - gender
-    responses:
-      201:
-        description: 회원가입 성공
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-            token:
-              type: string
-            nickname:
-              type: string
-            limited_access:
-              type: boolean
-      400:
-        description: 잘못된 요청
-      500:
-        description: 서버 에러
-    """
     try:
         data = request.get_json()
         nickname = data.get('nickname')
@@ -92,7 +40,7 @@ def signup():
         phone    = data.get('phone')
 
         if not nickname or age is None or not gender:
-            return json_kor({"error": "닉네임, 나이, 성별은 필수입니다."}), 400
+            return json_kor({"error": "닉네임, 나이, 성별은 필수입니다."}, 400)
 
         if db.user.find_one({"nickname": nickname}):
             return json_kor({"error": "이미 존재하는 닉네임입니다."}), 400
@@ -112,163 +60,94 @@ def signup():
         user_doc = db.user.find_one({"_id": result.inserted_id})
         token = create_token(user_doc)
 
-        return json_kor({
+        resp = make_response(json_kor({
             "message": "회원가입 성공!",
-            "token": token,
             "nickname": user_doc["nickname"],
             "limited_access": limited_access
-        }), 201
+        }, 201))
+        resp.set_cookie(
+            "access_token", token,
+            httponly=True, secure=True, samesite='Lax', max_age=60 * ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        return resp
     except Exception as e:
-        return json_kor({"error": str(e)}), 500
-
+        return json_kor({"error": str(e)}, 500)
 
 @user_test.route('/login', methods=['POST'])
 def login():
-    """
-    로그인
-    ---
-    tags:
-      - User
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          type: object
-          properties:
-            nickname:
-              type: string
-              description: 사용자 닉네임
-          required:
-            - nickname
-    responses:
-      200:
-        description: 로그인 성공
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-            token:
-              type: string
-            nickname:
-              type: string
-            limited_access:
-              type: boolean
-      400:
-        description: 닉네임 누락
-      404:
-        description: 사용자 없음
-      500:
-        description: 서버 에러
-    """
     try:
         data = request.get_json()
         nickname = data.get('nickname')
 
         if not nickname:
-            return json_kor({"error": "닉네임을 입력해주세요."}), 400
+            return json_kor({"error": "닉네임을 입력해주세요."}, 400)
 
         user_doc = db.user.find_one({"nickname": nickname})
         if not user_doc:
-            return json_kor({"error": "해당 닉네임의 사용자가 존재하지 않습니다."}), 404
+            return json_kor({"error": "해당 닉네임의 사용자가 존재하지 않습니다."}, 404)
 
         token = create_token(user_doc)
-        return json_kor({
+
+        resp = make_response(json_kor({
             "message": "로그인 성공!",
-            "token": token,
             "nickname": user_doc["nickname"],
             "limited_access": user_doc.get("limited_access", False)
-        }), 200
+        }))
+        resp.set_cookie(
+            "access_token", token,
+            httponly=True, secure=True, samesite='Lax', max_age=60 * ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        return resp
     except Exception as e:
-        return json_kor({"error": str(e)}), 500
+        return json_kor({"error": str(e)}, 500)
+
+@user_test.route('/logout', methods=['POST'])
+def logout():
+    resp = make_response(json_kor({"message": "로그아웃되었습니다."}))
+    resp.set_cookie("access_token", "", expires=0)
+    return resp
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
+        token = request.cookies.get('access_token')
         if not token:
-            return json_kor({"error": "토큰이 필요합니다."}), 401
+            return json_kor({"error": "쿠키에 토큰이 없습니다."}, 401)
         try:
-            token = token.split(" ")[1] if " " in token else token
             payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
             user_id = payload["user_id"]
             user = db.user.find_one({"_id": ObjectId(user_id)})
             if not user:
-                return json_kor({"error": "사용자를 찾을 수 없습니다."}), 404
+                return json_kor({"error": "사용자를 찾을 수 없습니다."}, 404)
             request.user = user
+            request.user_id = str(user["_id"])  # 다른 라우터에서도 사용 가능
         except jwt.ExpiredSignatureError:
-            return json_kor({"error": "토큰이 만료되었습니다."}), 401
+            return json_kor({"error": "토큰이 만료되었습니다."}, 401)
         except jwt.InvalidTokenError:
-            return json_kor({"error": "유효하지 않은 토큰입니다."}), 401
+            return json_kor({"error": "유효하지 않은 토큰입니다."}, 401)
         return f(*args, **kwargs)
     return decorated
-
 
 @user_test.route('/update', methods=['PATCH'])
 @token_required
 def update_user():
-    """
-    회원 정보 수정
-    ---
-    tags:
-      - User
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          type: object
-          properties:
-            nickname:
-              type: string
-            age:
-              type: integer
-            gender:
-              type: string
-            address:
-              type: string
-            phone:
-              type: string
-    responses:
-      200:
-        description: 수정 성공
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-            updated_user:
-              type: object
-      400:
-        description: 잘못된 요청
-      401:
-        description: 인증 실패
-      500:
-        description: 서버 에러
-    """
     try:
         user_id = request.user["_id"]
         data = request.get_json()
 
-        update_fields = {}
-        for field in ['nickname', 'age', 'gender', 'address', 'phone']:
-            if field in data:
-                update_fields[field] = data[field]
-
+        update_fields = {k: v for k, v in data.items() if k in ['nickname', 'age', 'gender', 'address', 'phone']}
         if "nickname" in update_fields:
-            existing = db.user.find_one({"nickname": update_fields["nickname"], "_id": {"$ne": user_id}})
-            if existing:
-                return json_kor({"error": "이미 존재하는 닉네임입니다."}), 400
+            if db.user.find_one({"nickname": update_fields["nickname"], "_id": {"$ne": user_id}}):
+                return json_kor({"error": "이미 존재하는 닉네임입니다."}, 400)
 
         if update_fields:
             db.user.update_one({"_id": user_id}, {"$set": update_fields})
 
         updated_user = db.user.find_one({"_id": user_id})
         updated_user["_id"] = str(updated_user["_id"])
-
         return json_kor({
             "message": "회원 정보가 성공적으로 수정되었습니다.",
             "updated_user": updated_user
-        }), 200
+        })
     except Exception as e:
-        return json_kor({"error": str(e)}), 500
+        return json_kor({"error": str(e)}, 500)

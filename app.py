f5934a94f63b4f3b4f7ecfd96992e9f75d484e16
rbanbla <kkhhyy25@hanmail.net>
2025-05-14 20:11:00 +0900

    배포 오류 수정

--- app.py
+++ app.py
@@ -90,9 +90,7 @@ def create_app():
         })
 
     return app
-
-
+app = create_app()
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_ENV", "development") == "development"
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=debug_mode)

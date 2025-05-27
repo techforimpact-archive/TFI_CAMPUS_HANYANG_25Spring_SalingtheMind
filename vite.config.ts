57c988245381de12d15ceb26ecb1d51eeeff51db
han-chaelin <chelin02@naver.com>
2025-05-28 04:45:21 +0900

    나의 보관함에서 편지+답장 불러오기인

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-/*import { defineConfig } from "vite";
+/*
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import legacy from "@vitejs/plugin-legacy";
@@ -20,6 +21,7 @@ export default defineConfig({
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)), } },
 });
 */
+
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
